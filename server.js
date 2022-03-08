7e3f9ae083ce56b56085f5bcefbe1401273eb91b
Sara Gochez <saragochez13@gmail.com>
2022-03-08 00:14:29 -0800

    created new routes folders and files, still getting errors

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const PORT = process.env.PORT || 3030;
 const app = express();
-
+const apiRoutes = require('./notesRoutes/apiRoutes');
 const notesRoutes = require('./notesRoutes/notes');
 
 
 app.use(express.static('./develop/public'));
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-app.use('/notes', notesRoutes);
 
+app.use('/notes', notesRoutes);
+app.use('/api/notes', apiRoutes);
 
 
 app.listen(PORT, () => {
