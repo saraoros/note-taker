6acaa465caea4c26dd317f04c3780afe5f70fb36
Sara Gochez <saragochez13@gmail.com>
2022-03-07 22:13:34 -0800

    fixed static path, html page now appears

--- notesRoutes/notes.js
+++ notesRoutes/notes.js
@@ -1,5 +1,8 @@
-const path = require("path");
 const router = require("express").Router();
+const fs = require('fs');
+const path = require('path');
+// const { notes } = require("../develop/db/db");
+
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../develop/public/notes.html"));
@@ -13,7 +16,17 @@ router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "../develop/public/notes.html"));
 });
 
-
+// examples from module: Would I need something like this?
+// router.post("/notes", (req, res) => {
+//     req.body.id = notes.length.toString();
+  
+//     if (!validateZookeeper(req.body)) {
+//       res.status(400).send("The zookeeper is not properly formatted.");
+//     } else {
+//       const zookeeper = createNewZookeeper(req.body, zookeepers);
+//       res.json(zookeeper);
+//     }
+//   });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3030;
 const app = express();
-const fs = require('fs');
-const path = require('path');
+
 const notesRoutes = require('./notesRoutes/notes');
 
 
-app.use(express.static('./public'));
-pp.use(express.urlencoded({ extended: true }));
+app.use(express.static('./develop/public'));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use('/notes', notesRoutes);
 
